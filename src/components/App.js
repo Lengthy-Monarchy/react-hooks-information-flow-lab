7608f7e82921ebb32fece2d25c31e7b07f9b5974
anne <anneirungu07@gmail.com>
2024-02-01 12:56:55 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from './components/header';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -9,15 +10,14 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+      <Header isDarkMode={isDarkMode} onDarkModeClick={onDarkModeClick} />
+      <ShoppingList items={itemData} selectedCategory={selectedCategory} onCategoryChange={handleCategoryChange} />
     </div>
   );
 }
---
+++ src/components/filter.js
@@ -0,0 +1,16 @@
+// src/components/Filter.js
+import React from 'react';
+
+function Filter({ onCategoryChange }) {
+    return (
+        <select name="filter" onChange={onCategoryChange}>
+
+            <option value="All">All</option>
+            <option value="Produce">Produce</option>
+            <option   option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+    </select>
+);
+}
+
+export default Filter;
